302f01309ce4c65e1655d50fc483a27a86bd280a
Alex Suraci <asuraci@pivotal.io>
2015-02-19 10:28:45 -0800

    clean up run.sh a bit, to work on os x
    Signed-off-by: Chris Brown <cbrown@pivotal.io>

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e -x
+
 [ "$1" ] || {
     echo "Specify which flavor of busybox to build:"
     ls -d */
@@ -10,7 +12,7 @@
         exit 1
     }
 
-sudo docker build -t tarmaker:$1 $1/tarmaker || {
+docker build -t tarmaker:$1 $1/tarmaker || {
     echo "Something went wrong. Aborting."
         exit 1
     }
@@ -18,19 +20,13 @@ sudo docker build -t tarmaker:$1 $1/tarmaker || {
 [ -f $1/tarmaker/rootfs.tar ] && mv $1/tarmaker/rootfs.tar $1/tarmaker/rootfs.tar.old
 [ -f $1/tarmaker/rootfs.tar.md5 ] && mv $1/tarmaker/rootfs.tar.md5 $1/tarmaker/rootfs.tar.md5.old
 
-sudo docker run --name builder-$1 tarmaker:$1
-sudo docker cp builder-$1:/tmp/rootfs.tar $1
-sudo docker cp builder-$1:/tmp/rootfs.tar.md5 $1
+docker run --name builder-$1 tarmaker:$1
+docker cp builder-$1:/tmp/rootfs.tar $1
+docker cp builder-$1:/tmp/rootfs.tar.md5 $1
 sudo chown 1000:1000 $1/rootfs*
 
 cd $1
-if md5sum --check rootfs.tar.md5; then
-    sudo docker rm -f builder-$1 &&\
-    sudo docker rmi tarmaker:$1 &&\
-    # We must wait until all removal is done before next step
-    sudo docker build -t busyboxplus:$1 .
-else
-    echo "Checksum failed. Aborting."
-    echo "Note: the tarmaker:$1 image and builder-$1 container have not been deleted."
-    exit 1
-fi
+
+docker rm -f builder-$1
+docker rmi tarmaker:$1
+docker build -t busyboxplus:$1 .
