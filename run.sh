50fa6ab0c86ce78ff5d8502b4116453a7bceede6
Alex Suraci <suraci.alex@gmail.com>
2015-03-31 14:50:51 -0700

    fix formatting

--- run.sh
+++ run.sh
@@ -3,19 +3,20 @@
 set -e -x
 
 [ "$1" ] || {
-    echo "Specify which flavor of busybox to build:"
-    ls -d */
-    exit 1
-    }
+  echo "Specify which flavor of busybox to build:"
+  ls -d */
+  exit 1
+}
+
 [ -d "$1" ] || {
-    echo "Could not find directory $1."
-        exit 1
-    }
+  echo "Could not find directory $1."
+  exit 1
+}
 
 docker build -t tarmaker:$1 $1/tarmaker || {
-    echo "Something went wrong. Aborting."
-        exit 1
-    }
+  echo "Something went wrong. Aborting."
+  exit 1
+}
 
 [ -f $1/tarmaker/rootfs.tar ] && mv $1/tarmaker/rootfs.tar $1/tarmaker/rootfs.tar.old
 [ -f $1/tarmaker/rootfs.tar.md5 ] && mv $1/tarmaker/rootfs.tar.md5 $1/tarmaker/rootfs.tar.md5.old
